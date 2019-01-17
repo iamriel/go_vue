42a01c923a04e9c8c0e13e4d114cab84ab4b3269
Rieljun Liguid <me@iamriel.com>
2019-01-17 10:22:08 +0800

    Add svg components to avoid issuing http request when using

---
+++ frontend/src/components/svgs/SvgSquaresBottom.vue
@@ -0,0 +1,19 @@
+<template>
+  <svg width="489px" height="225px" viewBox="0 0 489 225" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <!-- Generator: Sketch 51.3 (57544) - http://www.bohemiancoding.com/sketch -->
+    <desc>Created with Sketch.</desc>
+    <defs></defs>
+    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+      <g id="squares-bottom" fill-rule="nonzero">
+        <rect id="Rectangle-path" fill="#F8D188" transform="translate(438.450000, 50.700000) rotate(-180.000000) translate(-438.450000, -50.700000) " x="388.45" y="0.7" width="100" height="100" rx="15"></rect>
+        <rect id="Rectangle-path" fill="#C0D4CC" transform="translate(50.240000, 94.360000) rotate(-180.000000) translate(-50.240000, -94.360000) " x="0.24" y="44.36" width="100" height="100" rx="15"></rect>
+        <rect id="Rectangle-path" fill="#F6ADA4" transform="translate(288.580000, 184.160000) rotate(-180.000000) translate(-288.580000, -184.160000) " x="248.58" y="144.16" width="80" height="80" rx="14"></rect>
+      </g>
+    </g>
+  </svg>
+</template>
+<script>
+export default {
+  name: "SvgSquaresBottom"
+};
+</script>
---
+++ frontend/src/components/svgs/SvgSquaresTop.vue
@@ -0,0 +1,19 @@
+<template>
+  <svg width="465px" height="118px" viewBox="0 0 465 118" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <!-- Generator: Sketch 51.3 (57544) - http://www.bohemiancoding.com/sketch -->
+    <desc>Created with Sketch.</desc>
+    <defs></defs>
+    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+      <g id="squares-top" fill-rule="nonzero">
+        <rect id="Rectangle-path" fill="#F8D188" x="0" y="58" width="60" height="60" rx="8.5"></rect>
+        <rect id="Rectangle-path" fill="#F6ADA4" x="395" y="0" width="70" height="70" rx="10.89"></rect>
+        <rect id="Rectangle-path" fill="#C0D4CC" x="190" y="0" width="50" height="50" rx="7.89"></rect>
+      </g>
+    </g>
+  </svg>
+</template>
+<script>
+export default {
+  name: "SvgSquaresTop"
+};
+</script>
